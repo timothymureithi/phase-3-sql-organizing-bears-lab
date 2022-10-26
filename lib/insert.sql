641a9e06a571ee5a0576a077b91d1f62c77dc8c3
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-10-26 11:41:34 +0300

    Completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO bears 
+(name, age, sex, color, temperament, alive)
+
+VALUES 
+("Mr. Chocolate", 5, "M", "Brown", "Powerful", 1),
+("Rowdy", 10, "M", "White", "Shy", 1),
+("Tabitha", 8, "F", "Brown", "Terretorial", 1),
+("Sergeant Brow", 20, "M", "Black", "Direct", 0),
+("Melissa", 15, "F", "Brown", "Outgoing", 1),
+("Grinch", 9, "M", "White", "Outgoing", 1),
+("Wendy", 7, "F", "Brown", "Powerful", 1),
+(NULL, 5, "M", "Black", "Shy", 1);
+
